9e048c9279ee6300b1f8f5792acdacd4d1b848fb
itohti <itohti@gmail.com>
2023-07-28 12:07:56 -0400

    Initial Commit

---
+++ aDrift/aDriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  aDriftApp.swift
+//  aDrift
+//
+//  Created by Izdar Tohti on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct aDriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
